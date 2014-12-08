d3f417f1e7b6255caf70cb110df2ea5ed125296e
Mingquan Zheng <mzheng@veracode.com>
2014-12-08 14:46:28 -0500

    use junit

--- properties/src/test/java/example/springboot/SettingsTest.java
+++ properties/src/test/java/example/springboot/SettingsTest.java
@@ -1,13 +1,13 @@
 package example.springboot;
 
 import example.springboot.Settings.ConfiguredSettings;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.testng.Assert;
 
 /**
  * SettingsTest
